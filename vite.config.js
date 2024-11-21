038870b98fd43f8014aba7271efede35f1f664eb
wael0726 <wmbennabi05@gmail.com>
2024-11-21 18:18:52 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BlogApp/', // Correct base for GitHub Pages
+  base: 'BlogApp/', // Correct base for GitHub Pages
 });
